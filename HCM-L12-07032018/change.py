8e686dc36800e70b9db57f3e23d1a919b53ffba7
Lameco411 <lameco411@gmail.com>
2021-03-31 17:20:48 +0700

    Quy hoach dong

--- HCM-L12-07032018/change.py
+++ HCM-L12-07032018/change.py
@@ -1,1 +1,14 @@
 # bài toán liên quan tới đếm cách là bài toán sử dụng quy hoạch động
+f=open('change.inp')
+n,k = map(lambda x:int(x),f.readline().split())
+ks=[x+1 for x in range(k)]
+def change(n,k): 
+    def ways(amount,arrays,i):
+        if amount==0:return 1
+        if amount<0 or len(arrays)==0:return 0
+        if (amount,i) not in res:
+            res[(amount,i)]=ways(amount-arrays[-1],arrays,i)+ways(amount,arrays[:-1],i-1)
+        return res[(amount,i)]
+    res={}
+    return ways(n,k,len(k)-1)
+print(change(n,ks))
