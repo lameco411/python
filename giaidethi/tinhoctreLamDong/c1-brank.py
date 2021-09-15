0b4bd9fe0240b78a9dd1454ca87fc725993d4aa0
Lâm Eco <lameco411@gmail.com>
2021-09-15 21:05:49 +0700

    giai bai 1 tin hoc tre lam dong

--- giaidethi/tinhoctreLamDong/c1-brank.py
+++ giaidethi/tinhoctreLamDong/c1-brank.py
@@ -1,5 +1,42 @@
 f=open('c1-brank.inp')
 n,m=map(lambda x:int(x),f.readline().split())
 a=list(map(lambda x:x.split(),f.readlines()))
-print(n,m)
-print(a)
+array=[]
+for i in range(n):  
+    array.append({'rank': 0, 'number': 0, 'greater': 0, 'less': 0, 'last': False, 'first': False})
+def SoSanh(a,b):
+    a=a-1
+    b=b-1
+    array[a]["less"]+=1
+    array[b]["greater"]+=1
+    array[a]["number"]+=1
+    array[b]["number"]+=1
+    if array[a]["less"]+array[a]["greater"]==n-1:
+        array[a]["rank"]=array[a]["greater"]+1
+    if array[b]["less"]+array[b]["greater"]==n-1:
+        array[b]["rank"]=array[b]["greater"]+1
+    if array[a]["rank"]==n-1:
+        array[b]["last"]=True
+    if array[b]["rank"]==2:
+        array[a]["rank"]=True
+SoSanh(4,3)
+SoSanh(4,2)
+SoSanh(3,2)
+SoSanh(1,2)
+SoSanh(2,5)
+sohocsinhbietduocthuhang=0
+for x in array:
+    if x["rank"]!=0 or x["last"]==True or x["first"]==True:
+        sohocsinhbietduocthuhang+=1
+print(sohocsinhbietduocthuhang)
+
+'''
+Khởi tạo một mảng học sinh
+viết hàm để kiểm tra 2 số
+đưa những so sánh vào để kiểm tra
+sau mỗi lần đưa vào kiểm tra thì sẽ có những thay đổi trong mỗi học sinh như sau
+- hạng của chính mình
+- số lượng so sánh
+- là hạng chót khi người mình so sánh là áp chót và lớn hơn mình
+- là lạng nhất khi biết người mình so sánh là hạng 2 và nhỏ hơn mình
+'''
